7ef56208967941af48f850f5c81f3de8e3cdfd78
杨丰 <yeahyf@game7.cc>
2019-04-18 12:11:49 +0800

    增加日志启动提示信息

--- log/log.go
+++ log/log.go
@@ -9,6 +9,7 @@ import (
 var L seelog.LoggerInterface
 
 func SetLogConf(logConfig *string) {
+	fmt.Println("Start set log config ... ")
 	//初始化全局变量Logger为seelog的禁用状态，主要为了防止Logger被多次初始化
 	var err error
 	L = seelog.Disabled
