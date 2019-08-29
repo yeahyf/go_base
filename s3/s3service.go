16ce72b9d2da91d291e2a56d0e85c5e50bcde3e8
杨丰 <yeahyf@game7.cc>
2019-08-29 14:59:26 +0800

    新增获取对象列表接口！

--- s3/s3service.go
+++ s3/s3service.go
@@ -128,6 +128,7 @@ func GetS3ServiceAccessID(region, accessid, accesskey *string) (*s3.S3, error) {
 	return s3.New(sess), nil
 }
 
+//根据profile获取s3操作对象
 func GetS3ServiceProfile(region, profile *string) (*s3.S3, error) {
 	var s3Service *s3.S3
 	//创建会话，默认采用配置方式，区域直接硬编码
@@ -149,7 +150,31 @@ func GetS3ServiceProfile(region, profile *string) (*s3.S3, error) {
 	return s3.New(sess), nil
 }
 
+//使用默认的profile进行处理，profile为.aws目录下面配置的内容
 func GetS3Service(region *string) (*s3.S3, error) {
 	profile := "default"
 	return GetS3ServiceProfile(region, &profile)
+}
+
+//根据业务要求获取符合前缀要求的存放在aws s3中文件的列表
+func GetObjectsList(bucket *string, prefix *string, s3Service *s3.S3) ([]*string, error) {
+	input := &s3.ListObjectsInput{
+		Bucket: bucket,
+		Prefix: prefix,
+	}
+
+	output, err := s3Service.ListObjects(input)
+	if err != nil {
+		log.Errorf("Get List Object Error! prefix = %s", *prefix)
+		return nil, err
+	} else {
+		//只需要关心Key即可
+		size := len(output.Contents)
+		//定义一个字符串指针切片
+		result := make([]*string, 0, size)
+		for _, object := range output.Contents {
+			result = append(result, object.Key)
+		}
+		return result, nil
+	}
 }
