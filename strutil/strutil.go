2b27f5f28cb994ff0179a9d96ec2a9d2c697b7e4
杨丰 <yeahyf@game7.cc>
2019-12-31 16:32:58 +0800

    增加部分字符串工具方法

--- strutil/strutil.go
+++ strutil/strutil.go
@@ -27,3 +27,39 @@ func String2bytes(s *string) []byte {
 	h := [3]uintptr{x[0], x[1], x[1]}     // 构造三个指针数组
 	return *(*[]byte)(unsafe.Pointer(&h))
 }
+
+func SortString(list []*string) {
+	length := len(list)
+	for i := 0; i < length; i++ {
+		for j := i + 1; j < length; j++ {
+			if CompareString(list[i], list[j]) {
+				temp := list[i]
+				list[i] = list[j]
+				list[j] = temp
+			}
+		}
+	}
+}
+
+// 返回true str1>=str2, 返回false str1<str2
+func CompareString(str1, str2 *string) bool {
+	len1 := len(*str1)
+	len2 := len(*str2)
+	length := len1
+	if len1 > len2 {
+		length = len2
+	}
+	for i := 0; i < length; i++ {
+		if (*str1)[i] > (*str2)[i] {
+			return true
+		}
+		if (*str1)[i] < (*str2)[i] {
+			return false
+		}
+	}
+	if len1 >= len2 {
+		return true
+	} else {
+		return false
+	}
+}
