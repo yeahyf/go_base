dda40d9b932e79098eebe07758394e362e440323
杨丰 <yeahyf@game7.cc>
2021-11-22 14:28:12 +0800

    在zap中增加对时间的滚动的支持

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/yeahyf/go_base
 
-go 1.16
+go 1.17
 
 require (
 	github.com/aws/aws-sdk-go v1.40.50
@@ -9,10 +9,39 @@ require (
 	github.com/golang/protobuf v1.5.2
 	github.com/gomodule/redigo v1.8.5
 	github.com/jlaffaye/ftp v0.0.0-20210307004419-5d4190119067
+	github.com/lestrrat/go-file-rotatelogs v0.0.0-20180223000712-d3151e2a480f
 	github.com/pkg/sftp v1.13.4
 	github.com/smallnest/rpcx v1.6.11
 	go.mongodb.org/mongo-driver v1.7.2
 	go.uber.org/zap v1.19.1
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
+)
+
+require (
+	github.com/fastly/go-utils v0.0.0-20180712184237-d95a45783239 // indirect
+	github.com/fatih/color v1.10.0 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/golang/snappy v0.0.2 // indirect
+	github.com/jehiah/go-strftime v0.0.0-20171201141054-1d33003b3869 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/jonboulle/clockwork v0.2.2 // indirect
+	github.com/klauspost/compress v1.9.5 // indirect
+	github.com/kr/fs v0.1.0 // indirect
+	github.com/lestrrat/go-envload v0.0.0-20180220120943-6ed08b54a570 // indirect
+	github.com/lestrrat/go-strftime v0.0.0-20180220042222-ba3bf9c1d042 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/tebeka/strftime v0.1.5 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.0.2 // indirect
+	github.com/xdg-go/stringprep v1.0.2 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+	golang.org/x/text v0.3.6 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
 )
--- log/zap.go
+++ log/zap.go
@@ -1,4 +1,3 @@
-///zap日志封装
 package log
 
 //格式化为debug,info,error三种日志信息
@@ -7,8 +6,14 @@ package log
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"os"
+	"path"
+	"strings"
 	"time"
+
+	rotatelogs "github.com/lestrrat/go-file-rotatelogs"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
@@ -32,11 +37,15 @@ const (
 //LevelConfig 日志配置结构体
 type LevelConfig struct {
 	Filename   string `json:"logpath"`
-	MaxSIze    int    `json:"maxsize"`
+	MaxSize    int    `json:"maxsize"`
 	MaxBackups int    `json:"backups"`
 	MaxAge     int    `json:"maxage"`
 	Compress   bool   `json:"compress"`
 	Name       string `json:"name"`
+	//0默认为lumberjack，1 小时，2 分钟,3 天
+	Type int `json:"type"`
+	//滚动时间，type=1 单位:小时，type=2 单位:分钟, type=3 单位:天
+	Rotation int `json:"rotation"`
 }
 
 //Config 完整配置
@@ -133,16 +142,32 @@ func reSetLevel() {
 
 //initLogger 初始化具体的日志对象
 func initLogger(logConfig *LevelConfig) (logger *zap.Logger) {
-	hook := lumberjack.Logger{
-		Filename:   logConfig.Filename,   // 日志文件路径
-		MaxSize:    logConfig.MaxSIze,    // megabytes  M
-		MaxBackups: logConfig.MaxBackups, // 最多保留30个备份
-		MaxAge:     logConfig.MaxAge,     // days 天为单位
-		Compress:   logConfig.Compress,   // 是否压缩 disabled by default
-		LocalTime:  true,                 //使用本地时间，否则使用UTC时间
+	var hook io.Writer
+	logPath := path.Dir(logConfig.Filename)
+	//强制建立目录，如果目录存在，建立失败，不影响
+	_ = os.MkdirAll(logPath, os.ModePerm)
+	if !strings.HasSuffix(logConfig.Filename, ".log") {
+		panic("Filename must end as .log!")
 	}
-
-	writeSyncer := zapcore.AddSync(&hook)
+	if logConfig.Type == 0 {
+		lumberLog := lumberjack.Logger{
+			Filename:   logConfig.Filename,   // 日志文件路径
+			MaxSize:    logConfig.MaxSize,    // megabytes  M
+			MaxBackups: logConfig.MaxBackups, // 最多保留30个备份
+			MaxAge:     logConfig.MaxAge,     // days 天为单位
+			Compress:   logConfig.Compress,   // 是否压缩 disabled by default
+			LocalTime:  true,                 //使用本地时间，否则使用UTC时间
+		}
+		hook = &lumberLog
+	} else {
+		var err error
+		hook ,err = initRotateLog(logConfig)
+		if err != nil{
+			fmt.Println("Init Log Error",err)
+			panic(err)
+		}
+	}
+	writeSyncer := zapcore.AddSync(hook)
 	encoderConfig := zapcore.EncoderConfig{
 		TimeKey: "time",
 		// LevelKey:      "level",
@@ -175,6 +200,35 @@ func initLogger(logConfig *LevelConfig) (logger *zap.Logger) {
 		logger = zap.New(core, caller, development, callerSkip)
 	}
 	return
+}
+
+func initRotateLog(logConfig *LevelConfig) (io.Writer, error) {
+	var patternTemp string
+	var rotationTime time.Duration
+	switch logConfig.Type {
+	case 1:
+		{
+			patternTemp = "_%Y%m%d%H.log"
+			rotationTime = time.Hour * time.Duration(logConfig.Rotation)
+		}
+	case 2:
+		{
+			patternTemp = "_%Y%m%d%H%M.log"
+			rotationTime = time.Minute * time.Duration(logConfig.Rotation)
+		}
+	case 3:
+		{
+			patternTemp = "_%Y%m%d.log"
+			rotationTime = time.Hour * 24 * time.Duration(logConfig.Rotation)
+		}
+	}
+	pattern := strings.ReplaceAll(logConfig.Filename, ".log", patternTemp)
+	hook, err := rotatelogs.New(
+		pattern, rotatelogs.WithLinkName(logConfig.Filename),
+		rotatelogs.WithMaxAge(time.Hour*24*time.Duration(logConfig.MaxAge)), // 保存天数
+		rotatelogs.WithRotationTime(rotationTime),                           //切割频率 小时
+	)
+	return hook, err
 }
 
 //Debug 输出日志
@@ -210,7 +264,7 @@ func Errorf(format string, msg ...interface{}) {
 	errorLog.Error(fmt.Sprintf(format, msg...))
 }
 
-func Warn(msg ...interface{}){
+func Warn(msg ...interface{}) {
 	info := fmt.Sprint(msg...)
 	warnLog.Warn(fmt.Sprintf("%s", info))
 }
