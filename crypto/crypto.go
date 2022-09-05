a4024d6d7055e960442d0fbd6d4829d536861296
杨丰 <yeahyf@game7.cc>
2022-09-05 19:28:37 +0800

    增加md5处理接口

--- crypto/crypto.go
+++ crypto/crypto.go
@@ -7,7 +7,7 @@ import (
 	"github.com/yeahyf/go_base/strutil"
 )
 
-//MD5 获取字符串的MD5签名
+// MD5 获取字符串的MD5签名
 func MD5(str *string) *string {
 	h := md5.New()
 	h.Write(strutil.String2bytes(str))
@@ -15,9 +15,24 @@ func MD5(str *string) *string {
 	return &result
 }
 
+// MD5S 获取字符串的MD5签名
+func MD5S(str string) string {
+	h := md5.New()
+	h.Write(strutil.Str2bytes(str))
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+// MD54Bytes 获取字节数组的md5值
 func MD54Bytes(str []byte) *string {
 	h := md5.New()
 	h.Write(str)
 	result := hex.EncodeToString(h.Sum(nil))
 	return &result
+}
+
+// MD54Bs 获取字节数组的md5值
+func MD54Bs(str []byte) string {
+	h := md5.New()
+	h.Write(str)
+	return hex.EncodeToString(h.Sum(nil))
 }
